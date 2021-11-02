66359718dc15d92d5e4faccfc9c5e6acc9f77784
edukng <eduk.kng@gmail.com>
2021-11-02 09:04:15 -0300

    Resolução exercício string repetida

---
+++ src/main/java/exercicios/extras/StringRepetida.java
@@ -0,0 +1,12 @@
+package exercicios.extras;
+
+public class StringRepetida {
+
+    public int verificaRepeticaoDeStrings(String primeiraString, String segundaString) {
+        if (primeiraString.contains(segundaString)) {
+            String[] repetidas = primeiraString.split(segundaString);
+            return repetidas.length;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/com/desenvolve/tech/StringRepetidaTeste.java
@@ -0,0 +1,11 @@
+package com.desenvolve.tech;
+
+import exercicios.extras.StringRepetida;
+
+public class StringRepetidaTeste {
+    private static StringRepetida stringDeTeste = new StringRepetida();
+
+    public static void main(String[] args) {
+        System.out.println("" + stringDeTeste.verificaRepeticaoDeStrings("eduardoeduardoeduardoeduardo", "do"));
+    }
+}
